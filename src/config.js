942a658163956d034bed3a6dc0b06b6d9cacc3b2
sibananda <sibanandapatra413@gmail.com>
2025-08-16 13:04:03 +0530

    change in config

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 const config = {
     s3: {
     REGION: "us-east-1",
-    BUCKET: "hello-sibananda",
+    BUCKET: "notes-bucket-siba",
     },
     apiGateway: {
     REGION: "us-east-1",
-    URL: "https://dqbqp5lc59.execute-api.us-east-1.amazonaws.com/prod",
+    URL: "https://h5dyftflz3.execute-api.us-east-1.amazonaws.com/prod",
 },
 cognito: {
 REGION: "us-east-1",
-USER_POOL_ID: "us-east-1_CGRzoAKP6",
-APP_CLIENT_ID: "4ao55jlfkilr79c8v4ln6ug6rp",
-IDENTITY_POOL_ID: "us-east-1:943467f9-bf62-4b23-af33-09cbc5a8c8df",
+USER_POOL_ID: "us-east-1_uz11GiMgx",
+APP_CLIENT_ID: "p7v78d36fnb5btg109j0l5ijd",
+IDENTITY_POOL_ID: "us-east-1:df5c541d-b0b4-4066-8fdc-b963d5a4fa3e",
 },
 MAX_ATTACHMENT_SIZE: 5000000, // 5MB
 };
